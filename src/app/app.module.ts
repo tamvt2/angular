569253457860af62fff5561119674580df743caa
Tam Vũ <tamvt02.gcode@gmail.com>
2023-10-16 08:28:23 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentOverviewCliComponent } from './component-overview-cli/component-overview-cli.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'about', component: ComponentOverviewCliComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ComponentOverviewCliComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    exports: [RouterModule]
 })
 export class AppModule { }
